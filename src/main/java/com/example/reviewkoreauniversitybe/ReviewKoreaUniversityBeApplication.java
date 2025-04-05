1b1d2d5c16d6076cac66722d080a7b3a2c645af5
Nguyễn Viết Hiên <viethien123vn@gmail.com>
2025-04-05 15:57:27 +0900

    firt commit

---
+++ src/main/java/com/example/reviewkoreauniversitybe/ReviewKoreaUniversityBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewkoreauniversitybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewKoreaUniversityBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewKoreaUniversityBeApplication.class, args);
+    }
+
+}
